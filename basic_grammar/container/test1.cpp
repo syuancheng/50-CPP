c8e9dcd2de5bd948eb2eef2db67655c3b00f183e
siyuancheng <syuan.cheng21@gmail.com>
2023-02-01 21:17:39 +0800

    update

---
+++ basic_grammar/container/test1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+    //1.Define
+    cout<<"1. Define a vector:\n";
+    vector<int> v1;//空vector
+
+    unordered_map<string, string> m1;
+    
+    
+
+    return 0;
+}
